d30c56ce1fc1d6553e82d3305bdd50931837b18e
Deepak Shanmugam <deepaksrd94@gmail.com>
2017-11-04 19:12:45 -0500

    Converted to Python3

--- pkg/SemanticSearchEngine.py
+++ pkg/SemanticSearchEngine.py
@@ -5,8 +5,8 @@ Created on Oct 30, 2017
 '''
 import collections
 import io
+import json
 import os
-import urllib2
 
 from nltk import tokenize
 from nltk.tokenize import word_tokenize
@@ -20,14 +20,14 @@ class SemanticSearchEngine:
         data = self.readArticles(path)
         data = self.removeArticleTitle(data)
         indexSentenceMap = self.createIndexMap(data)
-        dFrame = pd.DataFrame(indexSentenceMap.items(), columns=['id', 'words'])
+        dFrame = pd.DataFrame(list(indexSentenceMap.items()), columns=['id', 'words'])
         jsonFileName = 'words.json'
         dFrame.to_json(jsonFileName, orient='records')
         return jsonFileName
         
     def readArticles(self, path):
         data = []
-        for f in sorted(os.listdir(path)):
+        for f in sorted(os.listdir(path), key=lambda x: int(x.split('.')[0])):
             with io.open(path + f, 'r', encoding='utf-8', errors='ignore') as dataFile:
                 data.append(dataFile.read())
         return data
@@ -46,30 +46,32 @@ class SemanticSearchEngine:
         for i in range(0, len(data)):
             for j in range(0, len(data[i])):
                 index = 'A' + str(i + 1) + 'S' + str(j + 1)
-                indexSentenceMap[index] = set(word_tokenize(data[i][j]))
+                indexSentenceMap[index] = list(set(word_tokenize(data[i][j])))
         return indexSentenceMap
         
         
-        
+ 
     # Refer https://github.com/Parsely/python-solr/blob/master/pythonsolr/pysolr.py 
     def indexWithSolr(self, jsonFileName):
         solr = pysolr.Solr('http://localhost:8983/solr/default')
         solr.delete(q='*:*')
         
         with open("/Users/deepaks/Documents/workspace/Semantic_Search_Engine/pkg/" + jsonFileName, 'rb') as jsonFile:
-            entry = jsonFile.read()
-        req = urllib2.Request('http://localhost:8983/solr/default/update/json?commit=true', entry)
-        req.add_header('Content-Type', 'application/json')
-        urllib2.urlopen(req)
-        # print(response.read())
+            entry = json.load(jsonFile)
+        solr.add(entry)
         return solr
     
+    def processQuery(self, query):
+        return list(set(word_tokenize(query)))
+    
     def searchInSolr(self, solr, query):
-        results = solr.search('words:' + query)
-        print("Top 10 documents with the term \"%s\"" % query)
+        tmp = ""
+        for item in query:
+            tmp = tmp + item + " "
+        results = solr.search(q="words:" + tmp)
+        print("Top 10 documents that closely match the query")
         for result in results:
             print(result['id'])
-        
         '''
         Another format for search
         '''
@@ -85,6 +87,6 @@ if __name__ == '__main__':
     path = '/Users/deepaks/Documents/workspace/Semantic_Search_Engine/Data/'
     jsonFileName = sse.corpusToJson(path)
     solr = sse.indexWithSolr(jsonFileName)
-    query = raw_input("Enter the input query: ")
-    sse.searchInSolr(solr, query)
-    
+    query = input("Enter the input query: ")
+    processedQuery = sse.processQuery(query)
+    sse.searchInSolr(solr, processedQuery)     
