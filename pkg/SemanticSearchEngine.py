6465ab812d66e44eca8eb44f5363747fa2ae5ab2
Deepak Shanmugam <deepaksrd94@gmail.com>
2017-11-15 01:26:02 -0600

    Fixed the bug in head word extraction

--- pkg/SemanticSearchEngine.py
+++ pkg/SemanticSearchEngine.py
@@ -3,6 +3,7 @@ Created on Oct 30, 2017
 
 @author: deepaks
 '''
+from _functools import reduce
 import collections
 import io
 import json
@@ -11,6 +12,7 @@ import os
 from nltk import pos_tag 
 from nltk import tokenize
 from nltk.corpus import wordnet as wn
+from nltk.parse.corenlp import CoreNLPDependencyParser
 from nltk.stem import WordNetLemmatizer
 from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import word_tokenize
@@ -28,9 +30,9 @@ class SemanticSearchEngine:
         indexWordsMap = self.createIndexMap(data)
         wordsDFrame = pd.DataFrame(list(indexWordsMap.items()), columns=['id', 'words'])
         
-        jsonFileName = 'words.json'
+        jsonFileName = 'Task2.json'
         wordsDFrame.to_json(jsonFileName, orient='records')
-        return indexWordsMap, jsonFileName
+        return data, indexWordsMap, wordsDFrame, jsonFileName
         
     def readArticles(self, path):
         data = []
@@ -56,13 +58,15 @@ class SemanticSearchEngine:
                 indexWordsMap[index] = list(set(word_tokenize(data[i][j])))
         return indexWordsMap
     
-    def extractFeatures(self, indexWordsMap):
+    def extractFeatures(self, data, indexWordsMap):
         indexLemmaMap = self.lemmatizeWords(indexWordsMap)
         lemmaDFrame = pd.DataFrame(list(indexLemmaMap.items()), columns=['id', 'lemmas'])
         indexStemMap = self.stemWords(indexWordsMap)
         stemDFrame = pd.DataFrame(list(indexStemMap.items()), columns=['id', 'stems'])
         indexPOSMap = self.tagPOSWords(indexWordsMap)
-        POSDFrame = pd.DataFrame(list(indexPOSMap.items()), columns=['id', 'POS'])  
+        POSDFrame = pd.DataFrame(list(indexPOSMap.items()), columns=['id', 'POS'])
+        indexHeadMap = self.findHeadWord(data)
+        HeadDFrame = pd.DataFrame(list(indexHeadMap.items()), columns=['id', 'head'])  
         indexHypernymMap = self.extractHypernyms(indexWordsMap)
         HypernymDFrame = pd.DataFrame(list(indexHypernymMap.items()), columns=['id', 'hypernyms'])
         indexHyponymMap = self.extractHyponyms(indexWordsMap)
@@ -71,18 +75,15 @@ class SemanticSearchEngine:
         MeronymDFrame = pd.DataFrame(list(indexMeronymMap.items()), columns=['id', 'meronyms'])
         indexHolonymMap = self.extractHolonyms(indexWordsMap)
         HolonymDFrame = pd.DataFrame(list(indexHolonymMap.items()), columns=['id', 'holonyms'])
+        dfList = [lemmaDFrame, stemDFrame, POSDFrame, HeadDFrame, HypernymDFrame, HyponymDFrame, MeronymDFrame, HolonymDFrame]
+        finalDFrame = reduce(lambda left, right: pd.merge(left, right, on='id'), dfList)
         
-        jsonFileList = ['words.json', 'lemmas.json', 'stems.json', 'pos.json', 'hypernym.json', 'hyponym.json', 'meronym.json', 'holonym.json']
-        lemmaDFrame.to_json(jsonFileList[1], orient='records')
-        stemDFrame.to_json(jsonFileList[2], orient='records')
-        POSDFrame.to_json(jsonFileList[3], orient='records')
-        HypernymDFrame.to_json(jsonFileList[4], orient='records')
-        HyponymDFrame.to_json(jsonFileList[5], orient='records')
-        MeronymDFrame.to_json(jsonFileList[6], orient='records')
-        HolonymDFrame.to_json(jsonFileList[7], orient='records')
-        return jsonFileList
+        jsonFileName = 'Task3.json'
+        finalDFrame.to_json(jsonFileName, orient='records')
+        return jsonFileName
     
     def lemmatizeWords(self, indexWordsMap):
+        print("Lemmatizing...")
         indexLemmaMap = collections.OrderedDict()
         wnl = WordNetLemmatizer()
         for k, v in indexWordsMap.items():
@@ -90,6 +91,7 @@ class SemanticSearchEngine:
         return indexLemmaMap
     
     def stemWords(self, indexWordsMap):
+        print("Stemming...")
         indexStemMap = collections.OrderedDict()
         stemmer = PorterStemmer()
         for k, v in indexWordsMap.items():
@@ -97,12 +99,29 @@ class SemanticSearchEngine:
         return indexStemMap
     
     def tagPOSWords(self, indexWordsMap):
+        print("POS Tagging...")
         indexPOSMap = collections.OrderedDict()
         for k, v in indexWordsMap.items():
-            indexPOSMap[k] = [pos_tag(word) for word in v]
+            indexPOSMap[k] = pos_tag(v)
         return indexPOSMap
     
+    def findHeadWord(self, data):
+        print("Head Word Extraction...")
+        indexHeadMap = collections.OrderedDict()
+        dependency_parser = CoreNLPDependencyParser('http://localhost:9000')
+        for i in range(0, len(data)):
+            for j in range(0, len(data[i])):
+                index = 'A' + str(i + 1) + 'S' + str(j + 1)
+                parsedSentence = list(dependency_parser.raw_parse(data[i][j]))[0]
+                rootValue = list(list(parsedSentence.nodes.values())[0]['deps']['ROOT'])[0]
+                for n in parsedSentence.nodes.values():
+                    if n['address'] == rootValue:
+                        indexHeadMap[index] = n['word']
+                        break
+        return indexHeadMap 
+    
     def extractHypernyms(self, indexWordsMap):
+        print("Hypernyms Extraction..")
         indexHypernymMap = collections.OrderedDict()
         for k, v in indexWordsMap.items():
             hypernymList = []
@@ -118,6 +137,7 @@ class SemanticSearchEngine:
         return indexHypernymMap
     
     def extractHyponyms(self, indexWordsMap):
+        print("Hyponyms Extraction..")
         indexHyponymMap = collections.OrderedDict()
         for k, v in indexWordsMap.items():
             hyponymList = []
@@ -133,6 +153,7 @@ class SemanticSearchEngine:
         return indexHyponymMap
     
     def extractMeronyms(self, indexWordsMap):
+        print("Meronyms Extraction..")
         indexMeronymMap = collections.OrderedDict()
         for k, v in indexWordsMap.items():
             meronymList = []
@@ -147,6 +168,7 @@ class SemanticSearchEngine:
         return indexMeronymMap
     
     def extractHolonyms(self, indexWordsMap):
+        print("Holonyms Extraction..")
         indexHolonymMap = collections.OrderedDict()
         for k, v in indexWordsMap.items():
             holonymList = []
@@ -162,6 +184,7 @@ class SemanticSearchEngine:
  
     # Refer https://github.com/Parsely/python-solr/blob/master/pythonsolr/pysolr.py 
     def indexWordsWithSolr(self, jsonFileName):
+        print("Indexing for Task2...")
         solr = pysolr.Solr('http://localhost:8983/solr/task2')
         solr.delete(q='*:*')
         
@@ -170,7 +193,7 @@ class SemanticSearchEngine:
         solr.add(entry)
         return solr
     
-    def processQuery(self, query):
+    def processQueryToExtractWords(self, query):
         return list(set(word_tokenize(query)))
     
     def searchInSolr(self, solr, query):
@@ -180,27 +203,128 @@ class SemanticSearchEngine:
         for result in results:
             print(result['id'])
     
-    def indexFeaturesWithSolr(self, jsonFileList):
+    def indexFeaturesWithSolr(self, jsonFileName):
+        print("Indexing for Task3...")
         solr = pysolr.Solr('http://localhost:8983/solr/task3')
         solr.delete(q='*:*')
         
-        for jsonFileName in jsonFileList:
-            with open("/Users/deepaks/Documents/workspace/Semantic_Search_Engine/pkg/" + jsonFileName, 'rb') as jsonFile:
-                entry = json.load(jsonFile)
-            solr.add(entry)
+        with open("/Users/deepaks/Documents/workspace/Semantic_Search_Engine/pkg/" + jsonFileName, 'rb') as jsonFile:
+            entry = json.load(jsonFile)
+        solr.add(entry)
         return solr
     
+    def processQueryToDoLemmatization(self, words):
+        lemmas = []
+        wnl = WordNetLemmatizer()
+        for word in words:
+            lemmas.append(wnl.lemmatize(word))
+        return lemmas
+    
+    def processQueryToDoStemming(self, words):
+        stems = []
+        stemmer = PorterStemmer()
+        for word in words:
+            stems.append(stemmer.stem(word))
+        return stems
+    
+    def processQueryToDoPOSTagging(self, words):
+        posTags = []
+        for word in words:
+            posTags.append(pos_tag(word))
+        return posTags
+    
+    def processQueryToExtractHeadWord(self, query):
+        dependency_parser = CoreNLPDependencyParser('http://localhost:9000')
+        headWord = None
+        parsedSentence = list(dependency_parser.raw_parse(query))[0]
+        rootValue = list(list(parsedSentence.nodes.values())[0]['deps']['ROOT'])[0]
+        for n in parsedSentence.nodes.values():
+            if n['address'] == rootValue:
+                headWord = n['word']
+                break
+        return headWord 
+    
+    def processQueryToExtractHypernyms(self, words):
+        hypernyms = []
+        for word in words:
+            '''Can use common Hypernyms for Task 4'''
+            synset = wn.synsets(word)
+            if len(synset) > 0:
+                if len(synset[0].hypernyms()) > 0:
+                    hypernyms.append(synset[0].hypernyms()[0].name().split('.')[0])
+                else:
+                    hypernyms.append(word)
+        return hypernyms
+    
+    def processQueryToExtractHyponyms(self, words):
+        hyponyms = []
+        for word in words:
+            '''Can use common Hyponyms for Task 4'''
+            synset = wn.synsets(word)
+            if len(synset) > 0:
+                if len(synset[0].hyponyms()) > 0:
+                    hyponyms.append(synset[0].hyponyms()[0].name().split('.')[0])
+                else:
+                    hyponyms.append(word)
+        return hyponyms
+    
+    def processQueryToExtractMeronyms(self, words):
+        meronyms = []
+        for word in words:
+            '''Can use common Meronyms for Task 4'''
+            synset = wn.synsets(word)
+            if len(synset) > 0:
+                if len(synset[0].part_meronyms()) > 0:
+                    meronyms.append(synset[0].part_meronyms()[0].name().split('.')[0])
+                else:
+                    meronyms.append(word)
+        return meronyms
+    
+    def processQueryToExtractHolonyms(self, words):
+        holonyms = []
+        for word in words:
+            '''Can use common Holonyms for Task 4'''
+            synset = wn.synsets(word)
+            if len(synset) > 0:
+                if len(synset[0].part_holonyms()) > 0:
+                    holonyms.append(synset[0].part_holonyms()[0].name().split('.')[0])
+                else:
+                    holonyms.append(word)
+        return holonyms
+    
+    def processQueryToExtractAllFeatures(self, query):
+        words = self.processQueryToExtractWords(query)
+        lemmas = self.processQueryToDoLemmatization(words)
+        stems = self.processQueryToDoStemming(words)
+        posTags = self.processQueryToDoPOSTagging(words)
+        headWord = self.processQueryToExtractHeadWord(query)
+        hypernyms = self.processQueryToExtractHypernyms(words)
+        hyponyms = self.processQueryToExtractHyponyms(words)
+        meronyms = self.processQueryToExtractMeronyms(words)
+        holonyms = self.processQueryToExtractHolonyms(words)
+        return [words, lemmas, stems, posTags, headWord, hypernyms, hyponyms, meronyms, holonyms]
+    
+    def searchInSolrWithMultipleFeatures(self,featuesList):
+        query = "words:" + " & words:".join(featuesList[0])
+        results = solr.search(query)
+        print("Top 10 documents that closely match the query")
+        for result in results:
+            print(result['id'])
+
     
 if __name__ == '__main__':
     sse = SemanticSearchEngine()
     path = '/Users/deepaks/Documents/workspace/Semantic_Search_Engine/Data/'
     # Task 2
-    indexWordsMap, jsonFileName = sse.preprocessCorpus(path)
-    solr = sse.indexWordsWithSolr(jsonFileName)
+    data, indexWordsMap, wordsDFrame, jsonFileName = sse.preprocessCorpus(path)
+    solr = sse.indexWordsWithSolr('Task2.json')
     query = input("Enter the input query: ")
-    processedQuery = sse.processQuery(query)
+    processedQuery = sse.processQueryToExtractWords(query)
     sse.searchInSolr(solr, processedQuery) 
     # Task 3
-    jsonFileList = sse.extractFeatures(indexWordsMap)
-    solr = sse.indexFeatureswithSolr(jsonFileList)
+#     jsonFileName = sse.extractFeatures(data, indexWordsMap)
+    solr = sse.indexFeaturesWithSolr('Task3.json')
+    query = input("Enter the input query: ")
+    featuresList = sse.processQueryToExtractAllFeatures(query)
+    sse.searchInSolrWithMultipleFeatures(solr, featuresList)
         
